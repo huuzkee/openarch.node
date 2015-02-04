8c8a8eca0fccd6b95cbfc340688fd11a6cf1f9c9
Marcelle von Wendland <mvw@hivves.com>
2015-02-04 21:27:59 +0000

    start01

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
     "net/http"
     "fmt"
     "log"
-    "os"
-    "io"
+    //"os"
+    //"io"
     "runtime"
 
     // go get github.com/gorilla/mux
     "github.com/gorilla/mux"
+    
+    
 )
 
 const (
@@ -38,6 +40,8 @@ func redirect(r *mux.Router, from string, to string){
     })
 }
 
+
+
 func main(){
     port := 9000
     ssl_port := 443
@@ -54,26 +58,47 @@ func main(){
     redirect(http_r, HOST, fmt.Sprintf("http://www.%s:%d", HOST, port))
 
     //  Redirect "http://secure.HOST" => "https://secure.HOST"
-    redirect(http_r, "secure."+HOST, fmt.Sprintf("https://secure.%s", HOST))
+   // redirect(http_r, "secure."+HOST, fmt.Sprintf("https://secure.%s", HOST))
+    redirect(http_r,  HOST, fmt.Sprintf("https://secure.%s", HOST))
 
-    www := http_r.Host("www."+HOST).Subrouter()
-    www.HandleFunc("/", Handler_www)
+    //www := http_r.Host("www."+HOST).Subrouter()
+    www := http_r.Host( HOST).Subrouter()
+    www.HandleFunc("/login/", Handler_www)
 
     api := http_r.Host("api."+HOST).Subrouter()
     api.HandleFunc("/", Handler_api)
 
-    secure := https_r.Host("secure."+HOST).Subrouter()
-    secure.HandleFunc("/", Handler_secure)
+    //secure := https_r.Host("secure."+HOST).Subrouter()
+   // secure.HandleFunc("/", Handler_secure)
+     secure := https_r.Host( HOST).Subrouter()
+    secure.HandleFunc("/sec/", Handler_secure)
 
     //  Start HTTP
-    err_http := http.ListenAndServe(fmt.Sprintf(":%d", port), http_r)
-    if err_http != nil {
-        log.Fatal("Web server (HTTP): ", err_http)
-    }
+    //err_http := http.ListenAndServe(fmt.Sprintf(":%d", port), http_r)
+    //if err_http != nil {
+    //    log.Fatal("Web server (HTTP): ", err_http)
+    //}
 
     //  Start HTTPS
-    err_https := http.ListenAndServeTLS(fmt.Sprintf(":%d", ssl_port), "D:/Go/src/www/ssl/public.crt", "D:/Go/src/www/ssl/private.key", https_r)
+    //err_https := http.ListenAndServeTLS(fmt.Sprintf(":%d", ssl_port), "C:\Users\\Marcelle\git\huuzlee\\openarch.node\cacert.pem", "C:\Users\Marcelle\git\huuzlee\openarch.node\private\aaacakey.pem", https_r)
+    err_https := http.ListenAndServeTLS(fmt.Sprintf(":%d", ssl_port), "C:\\Users\\Marcelle\\git\\huuzlee\\openarch.node\\localhost.pem", "C:\\Users\\Marcelle\\git\\huuzlee\\openarch.node\\localhost-key.pem", https_r)
     if err_https != nil {
-        log.Fatal("Web server (HTTPS): ", err_https)
-    }
+         log.Fatal("Web server (HTTPS): ", err_https)
+     }
+    
+    
+    //  Start HTTP
+    //go func() {
+    //    err_http := http.ListenAndServe(fmt.Sprintf(":%d", port), http_r)
+    //    if err_http != nil {
+    //        log.Fatal("Web server (HTTP): ", err_http)
+    //    }
+    // }()
+    
+    //  Start HTTPS
+    //err_https := http.ListenAndServeTLS(fmt.Sprintf(":%d", ssl_port),    "C:/Users/Marcelle/git/huuzlee/openarch.node/cacert.pem", "C:\\Users\\Marcelle\\git\\huuzlee\\openarch.node\\private\\cakey.pem", https_r)
+    //if err_https != nil {
+    //    log.Fatal("Web server (HTTPS): ", err_https)
+    //}
+
 }
